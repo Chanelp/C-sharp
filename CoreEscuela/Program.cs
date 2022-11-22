e960f7828e570ea902cee596c3ee1e097690e57e
Chanelp <mariannissanchez27@gmail.com>
2022-11-21 22:34:19 -0400

    Pilares básicos de POO

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,20 @@
+﻿//EJEMPLO SENCILLA PARA ENTENDER LOS PILARES BASE DE POO: CLASES, OBJETOS Y MODIFICADORES DE ACCESO PRINCIPALES
+
+var miEscuela = new CoreEscuela();
+miEscuela.nombre = "Platzi Academy";
+miEscuela.direccion = "Bogotá";
+miEscuela.añoFundación = 2017;
+Console.WriteLine("TIMBRAR");
+miEscuela.Timbrar();
+
+class CoreEscuela
+{
+    public string nombre;
+    public string direccion;
+    public int añoFundación;
+    public string director = "Freddy Vega";
+
+    public void Timbrar(){
+        Console.Beep(2000, 3000);
+    }
+}
---
+++ consoleapp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using System;
+using Humanizer;
+
+
+Console.WriteLine("Please enter a name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your rol");
+string rol = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+var age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"My name is {name}, my rol is {rol}, and I'm {age.ToWords(new System.Globalization.CultureInfo("en"))} years old");
+
+var text1 = DateHumanizeExtensions.Humanize(DateTime.Now,DateTime.UtcNow, new CultureInfo("es-MX")); 
+var text2 = DateTime.Now.AddMinutes(154).Humanize(culture: new CultureInfo("es-MX"));
+var text3 = DateTime.Now.AddMinutes(-468).Humanize(culture: new CultureInfo("es-MX"));
+
+System.Console.WriteLine(DateTime.Now);
+System.Console.WriteLine(text1);
+System.Console.WriteLine(text2);
+System.Console.WriteLine(text3);
