546084a24aa3a3325d60310d82d74c2ceccb7645
Chanelp <mariannissanchez27@gmail.com>
2022-11-22 16:33:34 -0400

    [ADD] Enumeraciones y override al método ToString

---
+++ Etapa1/Entidades/TipoEscuela.cs
@@ -0,0 +1,10 @@
+namespace CoreEscuela.Entidades {
+    //Los enumerados son un equivalente a tener una lista de enteros. Si no le colocamos un numero, se le asigna en orden 
+    public enum TipoEscuela
+    {
+        Primaria,
+        Secundaria,
+        Preescolar
+
+    }
+}
