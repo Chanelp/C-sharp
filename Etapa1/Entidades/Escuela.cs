6e60d5ecffa1ce2b3b2d211acb70f4ecd5bbc1f4
Chanelp <mariannissanchez27@gmail.com>
2022-11-22 16:39:20 -0400

    [ADD] StringBuilder

--- Etapa1/Entidades/Escuela.cs
+++ Etapa1/Entidades/Escuela.cs
@@ -1,31 +1,54 @@
+using System.Text;
+
 namespace CoreEscuela.Entidades
 {
-    class Escuela {
+    class Escuela
+    {
         string nombre; //Atributo
 
-        public string Nombre{ //Propiedades
+        public string Nombre
+        { //Propiedades
             get { return "Copia: " + nombre; }
             set { nombre = value.ToUpper(); }
         }
 
         public int AñoCreacion { get; set; }
         public string Pais { get; set; }
-        public string Ciudad {get; set;}
+        public string Ciudad { get; set; }
         public TipoEscuela TipoEscuela;
 
         //Constructor, método que inicializa el objeto
-        public Escuela(string nombre, int año){
+        public Escuela(string nombre, int año)
+        {
             this.nombre = nombre;
             AñoCreacion = año;
         }
-        
+
         //Sobreescribir método ToString()
-        public override string ToString(){
-            return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \n Pais: {Pais}, Año Creación: {AñoCreacion}, Ciudad: {Ciudad}" ;
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \n Pais: {Pais}, Año Creación: {AñoCreacion}, Ciudad: {Ciudad}";
         }
 
-        //Otra forma de definir el constructor
-        //public Escuela(string nombre, int año) => (Nombre, AñoCreacion) = (nombre, año);
+        /*
+        Otra forma de definir el constructor:
+        public Escuela(string nombre, int año) => (Nombre, AñoCreacion) = (nombre, año);
+        */
+
+        /*
+        Otra forma de retornar los atributos es con StringBuilder: mejora el performace y más específico.
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: " + this.Nombre + "\n");
+            sb.AppendLine("Pais: " + this.Pais + "\n");
+            sb.AppendLine("Ciudad: " + this.Ciudad + "\n");
+            sb.AppendLine("Año: " + this.AñoDeCreación + "\n");
+            sb.AppendLine("Tipo de Escuela: " + this.TipoEscuelas + "\n");
+            return sb.ToString();
+
+        }*/
 
     }
 }
