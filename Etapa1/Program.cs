35016aa450a5c45beada6a6ca3900dd455c277cd
Chanelp <mariannissanchez27@gmail.com>
2022-11-22 23:11:45 -0400

    [ADD] Entidades: Cursos, TipoJornada y generando ID únicos con guid

--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -1,9 +1,24 @@
 ﻿using CoreEscuela.Entidades;
 
-var miEscuela = new Escuela("ITLA", 2000, TipoEscuela.Secundaria);
-miEscuela.Pais = "Dominican Republic";
-miEscuela.Ciudad = "Santo Domingo";
+var escuela = new Escuela("ITLA", 2000, TipoEscuela.Secundaria);
+escuela.Pais = "Dominican Republic";
+escuela.Ciudad = "Santo Domingo";
 //miEscuela.TipoEscuela = TipoEscuela.Primaria;
 
-Console.WriteLine(miEscuela.Nombre);
-System.Console.WriteLine(miEscuela);
+
+//Instancias de escuela
+var curso1 = new Curso(){
+    Nombre = "101"
+};
+var curso2 = new Curso(){
+    Nombre = "201"
+};
+var curso3 = new Curso(){
+    Nombre = "301"
+};
+
+Console.WriteLine(escuela);
+System.Console.WriteLine(new String('=', 50));
+Console.WriteLine(curso1.Nombre + " , " + curso1.UniqueId);
+Console.WriteLine(curso2.Nombre + " , " + curso2.UniqueId);
+Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
